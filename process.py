489121a85ac55f192c6d3ae254b0c8277271b95e
zelima <ir.mchedlishvili@gmail.com>
2020-03-10 09:52:03 +0400

    [data]: join on country field as well to avoid mismatch

--- process.py
+++ process.py
@@ -30,10 +30,10 @@ Flow(
       set_type('Case', type='number', resources=None),
       join(
         source_name='time_series_19-covid-Confirmed',
-        source_key=['Province/State', 'Date'],
+        source_key=['Province/State', 'Country/Region', 'Date'],
         source_delete=True,
         target_name='time_series_19-covid-Deaths',
-        target_key=['Province/State', 'Date'],
+        target_key=['Province/State', 'Country/Region', 'Date'],
         fields=dict(Confirmed={
             'name': 'Case',
             'aggregate': 'first'
@@ -41,10 +41,10 @@ Flow(
       ),
       join(
         source_name='time_series_19-covid-Recovered',
-        source_key=['Province/State', 'Date'],
+        source_key=['Province/State', 'Country/Region', 'Date'],
         source_delete=True,
         target_name='time_series_19-covid-Deaths',
-        target_key=['Province/State', 'Date'],
+        target_key=['Province/State', 'Country/Region', 'Date'],
         fields=dict(Recovered={
             'name': 'Case',
             'aggregate': 'first'
